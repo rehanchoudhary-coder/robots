dc3eb24ec0df035293df7d1ab0a400647a86d593
saud-26 <anwarulhaquekhan619@gmail.com>
2026-04-21 23:49:35 +0530

    chore: stop tracking generated build output

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JPNWedqpkSU2uujgnDSsguFZ7SwAmLq5FgUhcn5tqk8=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JpD5Ge1UFgp328AkkPxElgXjWbx38oc3buMIFv4GRMw=\"\n}"
